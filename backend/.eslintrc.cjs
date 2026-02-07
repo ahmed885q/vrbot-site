ba50c3a5651667c72e65488ded0de98b0afb2ba9
ahmed885q <ahmed85q@hotmail.com>
2026-02-07 08:47:51 +0000

    feat(backend): bootstrap NestJS API + clean CI

--- backend/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  root: true,
-  env: { node: true, es2021: true, jest: true },
-  parser: "@typescript-eslint/parser",
-  parserOptions: { project: "./tsconfig.json", tsconfigRootDir: __dirname },
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
-  ignorePatterns: ["dist", "node_modules"],
-};
--- backend/test/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Test } from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
-
-describe('AppModule', () => {
-  it('should compile', async () => {
-    const moduleRef = await Test.createTestingModule({ imports: [AppModule] }).compile();
-    expect(moduleRef).toBeDefined();
-  });
-});
